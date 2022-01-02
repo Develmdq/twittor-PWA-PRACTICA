f84f62f14dcd938efb1e0b02130f14808fe07365
eledu <develmdq@gmail.com>
2022-01-02 15:06:00 -0300

    fix errors

--- server/server.js
+++ server/server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
 const app = express();
-
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 58253;
 
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+// app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // support encoded bodies
+
+
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // Directorio Público
